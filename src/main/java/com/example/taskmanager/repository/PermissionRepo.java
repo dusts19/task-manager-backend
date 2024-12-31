bc968c7bdc66b176166bae5451d1c064eeef693d
Dustin Shin <dustin.j.shin@gmail.com>
2024-12-31 01:01:36 -0800

    editted data.sql, changed name to NAME

--- src/main/java/com/example/taskmanager/repository/PermissionRepo.java
+++ src/main/java/com/example/taskmanager/repository/PermissionRepo.java
@@ -11,6 +11,3 @@ import com.example.taskmanager.model.Permission;
 public interface PermissionRepo extends JpaRepository<Permission, Long>{
 	Optional<Permission> findByName(String name);
 }
-//~[spring-boot-devtools-3.3.3.jar:3.3.3]
-//Caused by: org.postgresql.util.PSQLException: ERROR: insert or update on table "user_roles" violates foreign key constraint "fk6fql8djp64yp4q9b3qeyhr82b"
-//  Detail: Key (user_id)=(1) is not present in table "user_app".
