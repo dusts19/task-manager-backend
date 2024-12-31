ca3fd62c2e73740b470660f921d60f47876fa838
Dustin Shin <dustin.j.shin@gmail.com>
2024-12-31 02:18:43 -0800

    editted schema.sql, data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,21 +1,27 @@
-INSERT INTO PERMISSION (name) VALUES ('READ') ON CONFLICT (name) DO NOTHING;
-INSERT INTO PERMISSION (name) VALUES ('WRITE') ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO app_role (name) VALUES ('USER') ON CONFLICT (name) DO NOTHING;
+INSERT INTO app_role (name) VALUES ('USER') 
+ON CONFLICT (name) DO NOTHING;
+INSERT INTO app_role (name) VALUES ('ADMIN') 
+ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO app_role (name) VALUES ('ADMIN') ON CONFLICT (name) DO NOTHING;
+INSERT INTO Permission (name) VALUES ('READ') 
+ON CONFLICT (name) DO NOTHING;
+INSERT INTO Permission (name) VALUES ('WRITE') 
+ON CONFLICT (name) DO NOTHING;
 
 INSERT INTO role_permissions (role_id, permission_id)
 SELECT r.id, p.id FROM app_role r, Permission p 
-WHERE r.name = 'ADMIN' and p.name='' 
+WHERE r.name = 'ADMIN' and p.name='READ' 
 ON CONFLICT (role_id, permission_id) DO NOTHING;
 
 INSERT INTO role_permissions (role_id, permission_id)
 SELECT r.id, p.id FROM app_role r, Permission p 
-WHERE r.name = 'ADMIN' and p.name='' 
+WHERE r.name = 'ADMIN' and p.name='WRITE' 
 ON CONFLICT (role_id, permission_id) DO NOTHING;
 
-INSERT INTO app_user (username, password, email) VALUES ('admin', '$2a$10$oVfiGKiG06IvuKmFIMrwXeZEO4S24/ZE0yf8nDDlmYcmhN/wUaEeq', 'admin@example.com') ON CONFLICT (username) DO NOTHING;
+INSERT INTO app_user (username, password, email) 
+VALUES ('admin', '$2a$10$oVfiGKiG06IvuKmFIMrwXeZEO4S24/ZE0yf8nDDlmYcmhN/wUaEeq', 'admin@example.com') 
+ON CONFLICT (username) DO NOTHING;
 
 INSERT INTO user_roles (user_id, role_id) 
 SELECT u.id, r.id 
