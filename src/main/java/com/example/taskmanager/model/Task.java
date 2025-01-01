e84564fb5268a41fed2a8be5a499a92d13837550
Dustin Shin <dustin.j.shin@gmail.com>
2025-01-01 01:41:40 -0800

    editted tasks enitity, users fetchtype to LAZY

--- src/main/java/com/example/taskmanager/model/Task.java
+++ src/main/java/com/example/taskmanager/model/Task.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -41,7 +42,7 @@ public class Task {
 	private boolean taskcompleted;
 	
 //	@JsonBackReference
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id")
 	private User user;
 	
